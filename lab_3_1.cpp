44c4bda01f2d9181c8279c9d611b4bc907d02615
Unknown <suvimonthaiprasert898@gmail.com>
2017-11-28 18:01:39 +0700

    1-6

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,6 +4,21 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
-  cout << "Mr. Anukul" << endl;
+
+  cout << "Fullname : "<< "Miss "<< "Phanida Thaijamnong" << endl ;
+  cout << "Nickname : "<< "May" << endl ;
+  cout << "Gender : "<< "female" << endl ;
+  cout << "Date of birth : "<< "06 april 1999" << endl ;
+  cout << "Hobby : "<<  "Play sports and sing a song" << endl ;
+  cout << "Status : "<< "Single" <<endl ;
+
+  int a;
+  cout << "Hello World!" << "ABC" << endl;
+  cin >> a;
+  a = a + 14;
+  cout << "เราได้รับ";
+
+
+
   return 0;
 }
--- lab_3_4.cpp
+++ lab_3_4.cpp
@@ -19,7 +19,29 @@ int main() {
 
     // แก้ไขค่า
   	b = a * 5;
-  	c = a / 5;
+  #include <iostream>
+
+using namespace std;
+
+
+    int num1, num2, num3, num4, num5, sum;
+  	float avg;
+  	cout << "Enter First Number: ";
+  	cin >> num1;
+  	cout << "Enter Second Number: ";
+  	cin >> num2;
+  	cout << "Enter Third Number: ";
+  	cin >> num3;
+  	cout << "Enter Second Number: ";
+  	cin >> num4;
+  	cout << "Enter Third Number: ";
+  	cin >> num5;
+  	sum = num1 + num2 + num3 + num4 + num5;
+  	avg = (float)sum / 3;
+  	cout << "Average of the Three number is: ";
+  	cout << avg;
+  return 0;
+	c = a / 5;
   	d = a % 5;
 
     // แสดงข้อมูลหลังแก้ไข
@@ -30,6 +52,7 @@ int main() {
   	cout << d << endl;
 
     // เขียนโปรแกรมเพื่อหาค่า a = b * a + 20 * (20 - 15) / 10
+    a = b * a + 20 * (20 - 15) / 10;
 
 
     // เขียนแสดงผลค่า a
